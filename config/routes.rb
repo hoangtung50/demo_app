5b9a92e05caa818402430c53d8eec646626bb201
HoangTung <hoangtung@hoangtung-K43E.(none)>
2013-10-12 14:38:57 +0700

    Add contact.html.erb.Finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 DemoApp::Application.routes.draw do
   get "static_pages/home"
-
+  get "static_pages/contact"
   get "static_pages/help"
   get "static_pages/about"
   root :to => 'nguoi_dungs#index'
