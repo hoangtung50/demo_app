c8b5f284c1e68306027b722c570ca43266db43b8
HoangTung <hoangtung@hoangtung-K43E.(none)>
2013-10-12 11:48:55 +0700

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ DemoApp::Application.routes.draw do
   get "static_pages/home"
 
   get "static_pages/help"
-
+  get "static_pages/about"
   root :to => 'nguoi_dungs#index'
   resources :nguoi_dungs
 
