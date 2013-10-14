15a3a783d6b6b7b8601158e078ecb9c0b928dbfb
HoangTung <hoangtung@hoangtung-K43E.(none)>
2013-10-14 09:53:22 +0700

    Make a basic User model (including secure passwords)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131007023110) do
+ActiveRecord::Schema.define(:version => 20131014023354) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(:version => 20131007023110) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
