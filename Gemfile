d936f6cd70c04025959af5e76f5114b655d8d1b6
HoangTung <hoangtung@hoangtung-K43E.(none)>
2013-10-14 11:37:31 +0700

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -45,4 +45,5 @@ end
 group :test do
   # Pretty printed test output
   gem 'turn', '~> 0.8.3', :require => false
+  gem 'factory_girl_rails', '4.2.1'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 DemoApp::Application.routes.draw do
-  get "users/new"
-
-  #get "static_pages/home"
-  #get "static_pages/contact"
-  #get "static_pages/help"
-  #get "static_pages/about"
+  get "static_pages/home"
+  get "static_pages/contact"
+  get "static_pages/help"
+  get "static_pages/about"
   #root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/home',    to: 'static_pages#home',    via: 'get'
